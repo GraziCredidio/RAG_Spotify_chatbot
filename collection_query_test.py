9f7c15198aa6053d80a37feceff71d7859e1e5aa
Grazi Credidio <grazi.credidio@gmail.com>
2024-06-24 23:06:52 +0200

    Collection creation and collection testing

--- collection_query_test.py
+++ collection_query_test.py
@@ -1,33 +1,29 @@
+
 import chromadb
 from chromadb.utils import embedding_functions
 from review_data_etl import prepare_reviews_data
 from chroma_utils import build_chroma_collection
 
-
 DATA_PATH = "data/*"
 CHROMA_PATH = "review_embeddings"
-EMBEDDING_FUNC_NAME = "multi-qa-MiniLM-L6-cos-v1" # model specifically trained to solve question-and-answer semantic search tasks
+EMBEDDING_FUNC_NAME = "multi-qa-MiniLM-L6-cos-v1" 
 COLLECTION_NAME = "app_reviews"
 
-chroma_reviews_dict = prepare_reviews_data(DATA_PATH)
 
-build_chroma_collection(
-    CHROMA_PATH,
-    COLLECTION_NAME,
-    EMBEDDING_FUNC_NAME,
-    chroma_reviews_dict["ids"],
-    chroma_reviews_dict["documents"],
-    chroma_reviews_dict["metadatas"]
-)
-
-
+# Running query
 client = chromadb.PersistentClient(CHROMA_PATH)
 embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=EMBEDDING_FUNC_NAME)
 collection = client.get_collection(name=COLLECTION_NAME, embedding_function=embedding_func)
 
-great_reviews = collection.query(query_texts=["Find me some positive reviews that discuss the car's performance"],
+# Good reviews
+great_reviews = collection.query(query_texts=["Find me some positive reviews that discuss the apps performance"],
                                  n_results=5,
                                  include=["documents", "distances", "metadatas"])
 
 great_reviews["documents"][0][0]
-' Great all around car with great balance of performance and comfort. Terrific technology too.'
+
+# Bad reviews
+bad_reviews = collection.query(query_texts=["Find me reviews of people who did not like the app"],
+                                 n_results=5,
+                                 include=["documents", "distances", "metadatas"])
+bad_reviews["documents"][0][0]
